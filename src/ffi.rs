c6e8a5c50b9a73925bc6f62b669c737474989a0f
tiye <jiyinyiyong@gmail.com>
2026-08-28 13:27:10 +0800

    fix: remove obsolete paint ffi import

--- src/ffi.rs
+++ src/ffi.rs
@@ -1,4 +1,3 @@
 pub use calcit_native_ffi::{
-  invoke_blocking_callback, run_blocking_adapter, run_buffer_adapter, CalcitFfiAsyncTaskV1, CalcitFfiBlockingHostV1,
-  CalcitFfiBuffer,
+  invoke_blocking_callback, run_blocking_adapter, CalcitFfiAsyncTaskV1, CalcitFfiBlockingHostV1, CalcitFfiBuffer,
 };
